6eceaf7911d15e3a204c67ce25daa7bd9cf4626d
RafaelQSantos-RQS <rafaelqsantos33@gmail.com>
2023-12-21 16:13:48 -0300

    modified:   main.py 	modified:   modules/__pycache__/extract.cpython-310.pyc 	modified:   modules/extract.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from modules.extract import Extraction as ex
 
 #ut.setup_filesystem()
 list_of_pubmed = pd.read_csv('data/processed/pubmed_list.csv')['pubmed_accession_number']
-ex.extract_pmcid_from_pubmed(list_of_pubmed=list_of_pubmed,save_in='data/processed')
+ex.extract_pmcid_from_list(list_of_pubmed=list_of_pubmed,number_of_webscrappers=6,save_in='./data/processed')
--- modules/extract.py
+++ modules/extract.py
@@ -1,28 +1,42 @@
 import pandas as pd
 from playwright.sync_api import sync_playwright
+from concurrent.futures import ThreadPoolExecutor
+
+BASE_URL = 'https://pubmed.ncbi.nlm.nih.gov'
 
 class Extraction:
     '''
     '''
     @staticmethod
-    def extract_pmcid_from_pubmed(list_of_pubmed:list[str],save_in:str='.'):
+    def extract_pcmid(pubmed:str):
         '''
         '''
-        base_url = 'https://pubmed.ncbi.nlm.nih.gov'
-        dataframe = pd.DataFrame(columns=['pubmed_accession_number','pmcid'])
         with sync_playwright() as p:
             browser = p.firefox.launch(headless=False)
             context = browser.new_context()
             page = context.new_page()
-            for pubmed in list_of_pubmed:
-                url = f'{base_url}/{pubmed}'
-                page.goto(url=url)
-                if page.locator("#full-view-identifiers li").filter(has_text="PMCID").count() > 0:
-                    pmcid = page.locator("#full-view-identifiers li").filter(has_text="PMCID").inner_text().replace('PMCID: ','').strip()
-                    row = pd.Series({'pubmed_accession_number':str(pubmed),'pmcid':pmcid})
-                    dataframe = pd.concat([dataframe,row.to_frame().T],ignore_index=True)
-                else:
-                    row = pd.Series({'pubmed_accession_number':str(pubmed),'pmcid':None})
-                    dataframe = pd.concat([dataframe,row.to_frame().T],ignore_index=True)
-            dataframe.to_csv(save_in+'/pubmed_vs_pmcid.csv',index=False)
+            url = f'{BASE_URL}/{pubmed}'
+            page.goto(url=url)
+            if page.locator("#full-view-identifiers li").filter(has_text="PMCID").count() > 0:
+                pmcid = page.locator("#full-view-identifiers li").filter(has_text="PMCID").inner_text().replace('PMCID: ','').strip()
+                result =  {'pubmed_accession_number': str(pubmed), 'pmcid': pmcid}
+            else:
+                result = {'pubmed_accession_number': str(pubmed), 'pmcid': None}
+            
             browser.close()
+
+            return result
+
+    @staticmethod
+    def extract_pmcid_from_list(list_of_pubmed:list[str],number_of_webscrappers:int=1,save_in:str='.'):
+        '''
+        '''
+        dataframe = pd.DataFrame(columns=['pubmed_accession_number', 'pmcid'])
+        with ThreadPoolExecutor(max_workers=number_of_webscrappers) as executor:
+            results = list(executor.map(Extraction.extract_pcmid, list_of_pubmed))
+
+        for result in results:
+            row = pd.Series(result)
+            dataframe = pd.concat([dataframe,row.to_frame().T],ignore_index=True)
+        
+        dataframe.to_csv(save_in + '/pubmed_vs_pmcid.csv', index=False)
