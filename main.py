cb8a7cd39ab04227b6c0cf4e7cfc090f9acfc8c1
RafaelQSantos-RQS <rafaelqsantos33@gmail.com>
2023-12-22 12:37:58 -0300

    modified:   main.py 	modified:   modules/__pycache__/download.cpython-311.pyc 	modified:   modules/download.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ import modules.download as dw
 # Coletando os pmcid da lista de pubmed
 #ex.extract_pmcid_from_list(list_of_pubmed=list_of_pubmed,number_of_webscrappers=5,save_in='./data/processed')
 
-dw.download_article_from_pmcid(pmcid='PMC2263041')
+dw.download_article_from_pmcid(pmcid='PMC2263041',save_in='data/raw/articles')
--- modules/download.py
+++ modules/download.py
@@ -1,21 +1,24 @@
 from playwright.sync_api import sync_playwright
+import requests
 
 BASE_URL = 'https://www.ncbi.nlm.nih.gov'
 
-def download_article_from_pmcid(pmcid:str,save_in:str='./'):
+def download_article_from_pmcid(pmcid:str,save_in:str='.'):
     '''
     '''
     full_url = f'{BASE_URL}/pmc/articles/{pmcid}'
     with sync_playwright() as p:
-        browser = p.firefox.launch(headless=False,downloads_path='data/raw/articles')
+        browser = p.firefox.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
         page.goto(url=full_url)
         pdf_button = page.get_by_role("link", name="PDF (")
-        new_url = BASE_URL + pdf_button.get_attribute('href')
-        with page.goto(url=new_url).expect_download() as download_info:
-            
-        download = download_info.value
-
-        download.save_as(save_in + pmcid)
+        link_for_request = BASE_URL + pdf_button.get_attribute('href')
         browser.close()
+        headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(url=link_for_request,headers=headers)
+        with open(f'{save_in}/{pmcid}.pdf','wb') as file:
+            file.write(response.content)
+        print(response.headers.get('Content-Type'))
