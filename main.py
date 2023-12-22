1784fd6cbb17a87016860c5f478427c176d0bf1f
RafaelQSantos-RQS <rafaelqsantos33@gmail.com>
2023-12-22 18:20:43 -0300

    modified:   data_processing.ipynb 	modified:   main.py 	new file:   modules/__pycache__/download.cpython-310.pyc 	modified:   modules/__pycache__/extract.cpython-310.pyc 	modified:   modules/__pycache__/utilities.cpython-310.pyc 	modified:   modules/download.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import modules.extract as ex
 import modules.download as dw
 
 # Preparação do sistema de arquivos
-#utils.setup_filesystem()
+utils.setup_filesystem()
 
 # Extraindo a lista de pubmed
 #list_of_pubmed = pd.read_csv('data/processed/pubmed_list.csv')['pubmed_accession_number']
@@ -12,4 +12,7 @@ import modules.download as dw
 # Coletando os pmcid da lista de pubmed
 #ex.extract_pmcid_from_list(list_of_pubmed=list_of_pubmed,number_of_webscrappers=5,save_in='./data/processed')
 
-dw.download_article_from_pmcid(pmcid='PMC2263041',save_in='data/raw/articles')
+# Baixando os artigos
+#dw.download_article_from_pmcid(pmcid='PMC3559152',headless=True,save_in='data/raw')
+list_of_pmcid = set(pd.read_csv('data/processed/pubmed_vs_pmcid.csv')['pmcid'].dropna(axis=0))
+dw.download_articles_from_list_of_pmcid(list_of_pmcid=list_of_pmcid,headless=False,save_in='data/raw/articles',max_workers=4)
--- modules/download.py
+++ modules/download.py
@@ -1,24 +1,65 @@
-from playwright.sync_api import sync_playwright
 import requests
+import os
+from playwright.sync_api import sync_playwright
+from concurrent.futures import ThreadPoolExecutor
 
 BASE_URL = 'https://www.ncbi.nlm.nih.gov'
 
-def download_article_from_pmcid(pmcid:str,save_in:str='.'):
+def download_article_from_pmcid(pmcid: str, headless: bool = False, save_in: str = '.') -> None:
     '''
+    Baixa um artigo do PMC usando um PMC ID e salva-o como um arquivo PDF.
+
+    Parâmetros:
+        - pmcid (str): PMC ID do artigo.
+        - headless (bool): Indica se o navegador deve ser executado em modo headless.
+        - save_in (str): Diretório para salvar o arquivo PDF.
+
+    Retorna:
+        - None
     '''
     full_url = f'{BASE_URL}/pmc/articles/{pmcid}'
+    
     with sync_playwright() as p:
-        browser = p.firefox.launch(headless=False)
+        browser = p.firefox.launch(headless=headless)
         context = browser.new_context()
         page = context.new_page()
         page.goto(url=full_url)
-        pdf_button = page.get_by_role("link", name="PDF (")
-        link_for_request = BASE_URL + pdf_button.get_attribute('href')
-        browser.close()
-        headers = {
+        if page.get_by_role("link", name="PDF (").count() > 0:
+            pdf_button = page.get_by_role("link", name="PDF (")
+            link_for_request = BASE_URL + pdf_button.get_attribute('href')
+            
+            browser.close()
+            
+            headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
-        response = requests.get(url=link_for_request,headers=headers)
-        with open(f'{save_in}/{pmcid}.pdf','wb') as file:
-            file.write(response.content)
-        print(response.headers.get('Content-Type'))
+            }
+
+            response = requests.get(url=link_for_request, headers=headers)
+            response.raise_for_status()  # Verifica o status da resposta
+
+            with open(os.path.join(save_in, f'{pmcid}.pdf'), 'wb') as file:
+                file.write(response.content)
+
+            print(f"Artigo {pmcid} baixado com sucesso.")
+        else:
+            print(f"Falha ao baixar o artigo {pmcid}. PDF não disponível.")
+            with open(os.path.join(save_in, 'pmcid_não_baixados.txt'), 'a') as fail_file:
+                fail_file.write(f'{pmcid}\n')
+
+def download_articles_from_list_of_pmcid(list_of_pmcid: list[str], headless: bool, save_in: str = '.', max_workers: int = 1) -> None:
+    '''
+    Baixa artigos do PMC usando uma lista de PMC IDs em paralelo.
+
+    Parâmetros:
+        - list_of_pmcid (list[str]): Lista de PMC IDs dos artigos.
+        - headless (bool): Indica se o navegador deve ser executado em modo headless.
+        - save_in (str): Diretório para salvar os arquivos PDF.
+        - max_workers (int): Número máximo de threads para execução em paralelo.
+
+    Retorna:
+        - None
+    '''
+    headless_flags = [headless] * len(list_of_pmcid)
+    
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        executor.map(download_article_from_pmcid, list_of_pmcid, headless_flags, [save_in] * len(list_of_pmcid))
